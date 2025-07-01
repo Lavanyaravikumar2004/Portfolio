ce9ef70355c4c5fbfcd3692c27eec59dd4589087
Lavanya Ravikumar <lavanyaravikumar2004@gmail.com>
2025-07-01 23:08:04 +0530

    Add PWA manifest and service worker

--- todo-app/service-worker.js
+++ todo-app/service-worker.js
@@ -1,40 +1,18 @@
-const CACHE_NAME = "pro-todo-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./manifest.json"
-];
+const CACHE = "pro-todo-v1";
+self.addEventListener("install", e =>
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      "./",
+      "./index.html",
+      "./style.css",
+      "./script.js",
+      "./icon-192.png",
+      "./icon-512.png",
+      "./manifest.json"
+    ]))
+  )
+);
 
-// Install event
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Fetch event
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate event (optional for cleanup)
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
-  );
-});
+self.addEventListener("fetch", e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);

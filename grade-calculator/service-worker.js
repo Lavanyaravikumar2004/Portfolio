13cfceb935eab115bfb1f9be1ee29e1e4d6d6881
Lavanya Ravikumar <lavanyaravikumar2004@gmail.com>
2025-07-02 22:30:10 +0530

    🔧 Fix cache paths in service worker

--- grade-calculator/service-worker.js
+++ grade-calculator/service-worker.js
@@ -1,22 +1,22 @@
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open('grade-calculator-cache').then(function(cache) {
+    caches.open('grade-calculator-cache').then(function (cache) {
       return cache.addAll([
-        './',
-        './index.html',
-        './grade.css',
-        './grade.js',
-        './icon-192.png',
-        './icon-512.png',
-        './manifest.json'
+        '/grade-calculator/',
+        '/grade-calculator/index.html',
+        '/grade-calculator/grade.css',
+        '/grade-calculator/grade.js',
+        '/grade-calculator/icon-192.png',
+        '/grade-calculator/icon-512.png',
+        '/grade-calculator/manifest.json'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
